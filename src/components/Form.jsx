b8e9fe506fc7ee7704f9fc8d84a1b866eda674d3
Mrljsh <dragan.mrljes@vsar.edu.rs>
2022-12-22 16:17:02 +0100

    destructured form into sections, added Education section

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,9 @@ import { Component } from "react";
 class Form extends Component {
   handleFormSubmit = (e) => {
     const formData = new FormData(e.target);
+    // const educationObject = {
+    //   schoolName: formData.get("schoolName"),
+    // };
     const obj = {
       firstName: formData.get("firstName") ?? "",
       lastName: formData.get("lastName") ?? "",
@@ -10,8 +13,8 @@ class Form extends Component {
       mail: formData.get("mail") ?? "",
       phone: formData.get("phone") ?? "",
       address: formData.get("address") ?? "",
-      experience: formData.get("experience") ?? [],
-      education: formData.get("education") ?? [],
+      // experience: formData.get("experience") ?? [],
+      // education: educationObject ?? [],
     };
     this.props.handleSubmittedForm(obj);
   };
@@ -25,25 +28,59 @@ class Form extends Component {
             this.handleFormSubmit(e);
           }}
         >
-          <label htmlFor="firstName">
-            First name:
-            <input id="firstName" name="firstName"></input>
-          </label>
-          <label htmlFor="lastName">
-            Last name:
-            <input id="lastName" name="lastName"></input>
-          </label>
-          <label htmlFor="position">
-            Your position:
-            <input id="position" name="position"></input>
-          </label>
-          <label htmlFor="mail">
-            Mail address:
-            <input id="mail" name="mail"></input>
-          </label>
+          <PersonalInfoForm />
+          <EducationForm />
           <button type="submit">Submit</button>
         </form>
       </div>
+    );
+  }
+}
+
+class PersonalInfoForm extends Component {
+  render() {
+    return (
+      <section>
+        <h1>Your Personal Informations</h1>
+        <label htmlFor="firstName">
+          First name:
+          <input id="firstName" name="firstName"></input>
+        </label>
+        <label htmlFor="lastName">
+          Last name:
+          <input id="lastName" name="lastName"></input>
+        </label>
+        <label htmlFor="position">
+          Your position:
+          <input id="position" name="position"></input>
+        </label>
+        <label htmlFor="mail">
+          Mail address:
+          <input id="mail" name="mail"></input>
+        </label>
+        <label htmlFor="phone">
+          phone number:
+          <input id="phone" name="phone"></input>
+        </label>
+        <label htmlFor="address">
+          Address:
+          <input id="address" name="address"></input>
+        </label>
+      </section>
+    );
+  }
+}
+
+class EducationForm extends Component {
+  render() {
+    return (
+      <section>
+        <h1>Education</h1>
+        <label htmlFor="schoolName">
+          School name:
+          <input id="schoolName" name="schoolName" disabled></input>
+        </label>
+      </section>
     );
   }
 }
