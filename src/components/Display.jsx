0635a5acd700590c341b5444554241398d7f7092
Mrljsh <dragan.mrljes@vsar.edu.rs>
2022-12-29 17:53:59 +0100

    Added displaying experience, .education-item changed to section-item

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -35,11 +35,26 @@ class Display extends Component {
     <div>
       <h1>Education</h1>
       {this.props.data.education.map((education) => (
-        <div className="education-item">
+        <div className="section-item">
           <h1>{education.schoolName}</h1>
           <h2>{education.title}</h2>
           <p>
             ({education.dateStarted} - {education.dateFinished})
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+
+  ExperienceData = () => (
+    <div>
+      <h1>Experience</h1>
+      {this.props.data.experience.map((experience) => (
+        <div className="section-item">
+          <h1>{experience.companyName}</h1>
+          <h2>{experience.positionName}</h2>
+          <p>
+            ({experience.dateStarted} - {experience.dateFinished})
           </p>
         </div>
       ))}
@@ -55,6 +70,7 @@ class Display extends Component {
           <this.PersonalInfo />
           <hr />
           <this.EducationData />
+          <this.ExperienceData />
         </div>
       );
     }
